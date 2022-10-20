09c7af75c0b3d6d926e90308f53834fcf88cc254
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-10-19 21:19:34 -0400

    Tests finished.

--- test.py
+++ test.py
@@ -24,3 +24,35 @@ y = np.where(y == 'Iris-setosa', 0, 1)
 X = df.iloc[0:100, [0,2]].values
 
 # Testing Learning Rates
+fig, ax = plt.subplots(nrows=1, ncols=2,figsize=(10,4))
+
+# Learning Rate 0.1
+ada1 = adaline.AdalineGD(n_iter=15, eta=0.1).fit(X, y)
+ax[0].plot(range(1, len(ada1.losses_) +1), np.log10(ada1.losses_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Mean squared error)')
+ax[0].set_title('Adaline - learning rate 0.1')
+
+# Learning Rate 0.0001
+ada2 = adaline.AdalineGD(n_iter=15, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.losses_) +1), np.log10(ada2.losses_), marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('log(Mean squared error)')
+ax[1].set_title('Adaline - learning rate 0.0001')
+
+plt.show()
+
+# Standardizing Data
+X_std = np.copy(X)
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean())/X[:,0].std()
+
+
+# Graphing Standardized Data
+ada_gd = adaline.AdalineGD(n_iter=20, eta=0.5)
+ada_gd.fit(X_std, y)
+plt.plot(range(1, len(ada_gd.losses_) + 1), ada_gd.losses_, marker="o")
+plt.xlabel('Epochs')
+plt.ylabel('Mean Squared Error')
+plt.tight_layout()
+plt.show()
